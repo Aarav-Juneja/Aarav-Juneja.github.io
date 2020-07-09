19a336bd3299de9d75b1c28b653fe891ece58229
Aarav-Juneja <61629581+Aarav-Juneja@users.noreply.github.com>
2020-07-08 22:04:37 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,4 +15,4 @@ workbox.routing.registerRoute(
   ({request}) => request.destination === 'document',
   new workbox.strategies.NetworkFirst()
 );
-new workbox.strategies.NetworkFirst()
+//new workbox.strategies.NetworkFirst()
