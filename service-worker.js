8bd7e38bee2b76a4ad8aa9c6deec08149514021c
Aarav-Juneja <61629581+Aarav-Juneja@users.noreply.github.com>
2020-11-08 18:40:45 -0800

    Debug support
    Logging for debugging

--- service-worker.js
+++ service-worker.js
@@ -29,6 +29,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(
     (async () => {
       let data = caches.open("data")
+      console.log(data, caches)
       // Enable navigation preload if it's supported.
       // See https://developers.google.com/web/updates/2017/02/navigation-preload
       if ("navigationPreload" in self.registration) {
@@ -45,6 +46,7 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", event => {
   // the data cache
   let data = caches.open("data")
+  console.log(data, caches)
   // the url
   const url = new URL(event.request.url);
   // We only want to call event.respondWith() if this is a navigation request
